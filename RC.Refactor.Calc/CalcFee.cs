adced60ff564c96c719913149b011c26c6c8fdde
Richard Coady <rcoady@gmail.com>
2022-12-01 19:28:59 +0000

    Rules as  lambdas

--- RC.Refactor.Calc/CalcFee.cs
+++ RC.Refactor.Calc/CalcFee.cs
@@ -28,21 +28,22 @@
     /// </summary>
     public class UK : ICaculateFee<FeeVariables>
     {
-        public Dictionary<int, decimal> feeRules { get; set; } = new();
+        public Dictionary<decimal, Func<FeeVariables, bool>> _feeRules { get; set; } = new();
+
         public decimal Caculate(FeeVariables feeVariables)
         {
-            if (feeVariables.revenue < 100000)
-                return 0;
-            if (feeVariables.membership == "X")
-                return 100.00M;
-            if (feeVariables.membership == "X" && feeVariables.revenue > 100000)
-                return 200.00M;
-            if (feeVariables.membership == "X" && feeVariables.revenue < 100010 && feeVariables.membershipBand == "A")
-                return 300.00M;
-            if (feeVariables.membership == "X" && feeVariables.revenue > 100000 && feeVariables.membershipBand == "B")
-                return 300.00M;
-            if (feeVariables.membership == "X" && feeVariables.revenue > 12100 && feeVariables.membershipBand == "F")
-                return 300.00M;
+            //Rules as a lambda
+            _feeRules.Add(100, f => f.revenue < 100000);
+            _feeRules.Add(200, f => feeVariables.membership == "X");
+            _feeRules.Add(300, f => feeVariables.membership == "X" && feeVariables.revenue < 100010 && feeVariables.membershipBand == "A");
+            _feeRules.Add(400, f => f.revenue > 100000);
+            foreach (var rule in _feeRules)
+            {
+                if ((bool)rule.Value.DynamicInvoke(feeVariables))
+                {
+                    return rule.Key;
+                }
+            }
             //Assuming up to 10 rules at least
             return 0;
         }
